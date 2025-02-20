ab075b0a807a06465bd5bf7c9986a48673279260
Ankit yadav <ay870421@gmail.com>
2025-02-20 23:53:56 +0530

    Update server.js
    path removed

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import path from 'path'; // to work with paths
 import ProjectApi from './routes/ProjectApi.js';
 import testimonialRoutes from "./routes/testimonialRoute.js"
 import VisitorRoute from "./routes/VisitorApi.js";
